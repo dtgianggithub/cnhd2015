6e3ec74fba1399beee3bf3dacc0d2517d0995a86
LhGiang <lhgiang94@gmail.com>
2015-12-13 16:53:32 +0700

    Second commit

---
+++ API/ESApi/ESApi/Controllers/ExpController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ESApi.Controllers
+{
+    public class ExpController : Controller
+    {
+        //
+        // GET: /Exp/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
