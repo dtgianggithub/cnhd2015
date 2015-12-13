a3c06b256c759f4bda970b0aab8a80b3dbf776ae
hunghau <nguyenhunghau.us@gmail.com>
2015-12-13 17:49:25 +0700

    Add API

---
+++ API/ESApi/ESApi/Controllers/ProductAPIController.cs
@@ -0,0 +1,24 @@
+﻿using ESApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ESApi.Controllers
+{
+    public class ProductAPIController : ApiController
+    {
+        ESDBEntities db = new ESDBEntities();
+
+        [HttpGet]
+        [Route("api/product/all")]
+        public IHttpActionResult GetAllProducts()
+        {
+                List<SANPHAM> list = db.SANPHAMs.ToList();
+
+                return Ok(list);
+        }
+    }
+}
