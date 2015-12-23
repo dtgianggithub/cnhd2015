65d79bbf6c090d89de3a40c2cbb6339a0e44ce07
hunghau <nguyenhunghau.us@gmail.com>
2015-12-23 10:18:51 +0700

    API version 2

--- API/ESApi/ESApi/Controllers/ProductAPIController.cs
+++ API/ESApi/ESApi/Controllers/ProductAPIController.cs
@@ -13,15 +13,88 @@ namespace ESApi.Controllers
     public class ProductAPIController : ApiController
     {
         ESDBEntities db = new ESDBEntities();
+
         [HttpGet]
         [Route("api/product/all")]
         public IHttpActionResult GetAllProducts()
         {
-                List<SANPHAM> list = db.SANPHAMs.ToList();
+                List<SANPHAM> list = db.SANPHAMs.Where(sp => sp.DAXOA == false).ToList();
                 Mapper.CreateMap<SANPHAM, SANPHAMModel>();
                 List<SANPHAMModel> ret =
                     Mapper.Map<List<SANPHAM>, List<SANPHAMModel>>(list);
                 return Ok(ret);
+        }
+
+        [HttpGet]
+        [Route("api/product/ByID/{ID}")]
+        public IHttpActionResult GetProductbyID(string ID)
+        {
+            SANPHAM sanpham = db.SANPHAMs.Where(sp => sp.DAXOA == false && sp.MA == ID).SingleOrDefault();
+            Mapper.CreateMap<SANPHAM, SANPHAMModel>();
+            SANPHAMModel spmodel =
+                Mapper.Map<SANPHAM, SANPHAMModel>(sanpham);
+            return Ok(spmodel);
+        }
+
+        [HttpGet]
+        [Route("api/product/ByLSP/{LOAISANPHAM}")]
+        public IHttpActionResult GetProduct(int LOAISANPHAM)
+        {
+            List<SANPHAM> list = db.SANPHAMs.Where(sp => sp.DAXOA == false && sp.LOAISANPHAM == LOAISANPHAM).ToList();
+            Mapper.CreateMap<SANPHAM, SANPHAMModel>();
+            List<SANPHAMModel> ret =
+                Mapper.Map<List<SANPHAM>, List<SANPHAMModel>>(list);
+            return Ok(ret);
+        }
+
+        [HttpGet]
+        [Route("api/product/ByNSX/{NHASANXUAT}")]
+        public IHttpActionResult GetProductbyNSX(int NHASANXUAT)
+        {
+            List<SANPHAM> list = db.SANPHAMs.Where(sp => sp.DAXOA == false && sp.NHASANXUAT == NHASANXUAT).ToList();
+            Mapper.CreateMap<SANPHAM, SANPHAMModel>();
+            List<SANPHAMModel> ret =
+                Mapper.Map<List<SANPHAM>, List<SANPHAMModel>>(list);
+            return Ok(ret);
+        }
+
+        [HttpPost]
+        [Route("api/product/add")]
+        public IHttpActionResult AddProduct([FromBody] SANPHAM sp)
+        {
+            db.SANPHAMs.Add(sp);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("api/product/update")]
+        public IHttpActionResult UpdateProduct([FromUri] string Ma, [FromBody] SANPHAM s)
+        {
+            SANPHAM sanpham = db.SANPHAMs.Where(sp => sp.DAXOA == false && sp.MA == Ma).SingleOrDefault();
+            sanpham.MA = s.MA;
+            sanpham.TEN = s.TEN;
+            sanpham.DONGIABAN = s.DONGIABAN;
+            sanpham.MOTA = s.MOTA;
+            sanpham.SOLUONG = s.SOLUONG;
+            sanpham.LOAISANPHAM = s.LOAISANPHAM;
+            sanpham.SANPHAMMOI = s.SANPHAMMOI;
+            sanpham.SANPHAMMOI = s.SANPHAMMOI;
+            sanpham.SANPHAMBANCHAY = s.SANPHAMBANCHAY;
+            sanpham.DAXOA = s.DAXOA;
+            sanpham.MAKHUYENMAI = s.MAKHUYENMAI;
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("api/product/delete")]
+        public IHttpActionResult DeleteProduct([FromUri] string Ma)
+        {
+            SANPHAM sanpham = db.SANPHAMs.Where(sp => sp.DAXOA == false && sp.MA == Ma).SingleOrDefault();
+            db.SANPHAMs.Remove(sanpham);
+            db.SaveChanges();
+            return Ok();
         }
     }
 }
---
+++ API/ESApi/ESApi/Controllers/PromotionAPIController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using ESApi.Models;
+using ESApi.Models.ModelEntity;
+
+namespace ESApi.Controllers
+{
+    public class PromotionAPIController : ApiController
+    {
+        ESDBEntities db = new ESDBEntities();
+
+        [HttpGet]
+        [Route("api/promotion/all")]
+        public IHttpActionResult GetAllPromotion()
+        {
+            List<KHUYENMAI> list = db.KHUYENMAIs.Where(sp => sp.DAXOA == false).ToList();
+            Mapper.CreateMap<KHUYENMAI, KHUYENMAIModel>();
+            List<KHUYENMAIModel> ret =
+                Mapper.Map<List<KHUYENMAI>, List<KHUYENMAIModel>>(list);
+            return Ok(ret);
+        }
+        [HttpPost]
+        [Route("api/promotion/add")]
+        public IHttpActionResult AddPromotion([FromBody] KHUYENMAI km)
+        {
+            db.KHUYENMAIs.Add(km);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("api/promotion/update")]
+        public IHttpActionResult UpdateProduct([FromUri] int Ma, [FromBody] KHUYENMAI s)
+        {
+            KHUYENMAI khuyenmai = db.KHUYENMAIs.Where(sp => sp.DAXOA == false && sp.MA == Ma).SingleOrDefault();
+            khuyenmai.MA = s.MA;
+            khuyenmai.NGAYBATDAU = s.NGAYBATDAU;
+            khuyenmai.NGAYKETTHUC = s.NGAYKETTHUC;
+            khuyenmai.NOIDUNG = s.NOIDUNG;
+            khuyenmai.DAXOA = s.DAXOA;
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("api/promotion/delete")]
+        public IHttpActionResult DeleteTypeProduct([FromUri] int Ma)
+        {
+            KHUYENMAI khuyenmai = db.KHUYENMAIs.Where(sp => sp.DAXOA == false && sp.MA == Ma).SingleOrDefault();
+            db.KHUYENMAIs.Remove(khuyenmai);
+            db.SaveChanges();
+            return Ok();
+        }
+
+
+    }
+}
