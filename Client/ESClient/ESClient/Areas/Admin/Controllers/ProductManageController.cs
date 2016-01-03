172141a451c5c093789a527775f56812e7076d2a
hunghau <nguyenhunghau.us@gmail.com>
2016-01-03 20:40:44 +0700

    client
    I have just update angularjs in admin

---
+++ Client/ESClient/ESClient/Areas/Admin/Controllers/ProductManageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ESClient.Areas.Admin.Controllers
+{
+    public class ProductManageController : Controller
+    {
+        // GET: Admin/ProductManage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
